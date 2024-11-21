386089e894f787869cbe4ffa2f176765cb9c09f5
Ianistoc <ianis@lajonchere.org>
2024-11-21 23:45:27 +0100

    Finalized presentation

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-var pages = ["index.html", "summary.html", "objectives.html", "forces.html"];
+var pages = ["index.html", "summary.html", "objectives.html", "forces.html", "plan.html", "preventions.html", "points.html"];
 var current_page = 0;
 switch (window.location.href.split('/').pop()) {
-    case "index.html":
+    case pages[0]:
         current_page = 0;
         break;
 
-    case "summary.html":
+    case pages[1]:
         current_page = 1;
         break;
 
-    case "objectives.html":
+    case pages[2]:
         current_page = 2;
         break;
 
-    case "forces.html":
+    case pages[3]:
         current_page = 3;
+        break;
+    
+    case pages[4]:
+        current_page = 4;
+        break;
+    
+    case pages[5]:
+        current_page = 5;
+        break;
+    
+    case pages[6]:
+        current_page = 6;
         break;
 }
 document.onkeydown = function(e) {
