d2624817e1d819ea18071aa39918e1c4b32e710e
Ianistoc <ianis@lajonchere.org>
2024-11-28 18:13:11 +0100

    Added sliding animations

--- script.js
+++ script.js
@@ -1,52 +1,16 @@
 var pages = ["index.html", "summary.html", "objectives.html", "forces.html", "plan.html", "preventions.html", "points.html", "proud.html", "conclusion.html"];
-var current_page = 0;
-switch (window.location.href.split('/').pop()) {
-    case pages[0]:
-        current_page = 0;
-        break;
+var current_page = pages.indexOf(window.location.href.split('/').pop());
 
-    case pages[1]:
-        current_page = 1;
-        break;
-
-    case pages[2]:
-        current_page = 2;
-        break;
-
-    case pages[3]:
-        current_page = 3;
-        break;
-    
-    case pages[4]:
-        current_page = 4;
-        break;
-    
-    case pages[5]:
-        current_page = 5;
-        break;
-    
-    case pages[6]:
-        current_page = 6;
-        break;
-    
-    case pages[7]:
-        current_page = 7;
-        break;
-
-    case pages[8]:
-        current_page = 8;
-        break;
-}
 document.onkeydown = function(e) {
-    if(e.key === "ArrowRight") {
-        if(current_page < pages.length - 1){
-            window.location.href = pages[current_page + 1];
-        }
-    }
+    if (e.key === "ArrowRight" && current_page < pages.length - 1) {
+        document.getElementById("page1").style.transform = 'translateX(-100%)';
+        document.getElementById("page2").style.transform = 'translateX(0)';
 
-    else if(e.key === "ArrowLeft") {
-        if(current_page > 0) {
-            window.location.href = pages[current_page - 1];
-        }
+        setTimeout(function() {
+            window.location.href = pages[current_page + 1];
+        }, 1000);
+    } 
+    else if (e.key === "ArrowLeft" && current_page > 0) {
+        window.location.href = pages[current_page - 1];
     }
 }
